36fdded939935d36e3206a2947cc58761996d851
gulybyte <gulybyte@gmail.com>
2023-02-12 01:04:00 -0300

    waitAndNotify

--- src/four/waitAndNotify/Application.java
+++ src/four/waitAndNotify/Application.java
@@ -1,5 +1,11 @@
 package four.waitAndNotify;
 
+/*
+ * Aguarde e notifique em Java; métodos de multithreading de baixo nível do
+ * objeto classe que permite que você tenha um ou mais threads dormindo,
+ * apenas para ser acordado por outros tópicos no momento certo.
+ * Extremamente útil para evitar os loops de pesquisa que consomem o processador.
+ */
 public class Application {
 
     public static void main(String[] args) throws InterruptedException {
